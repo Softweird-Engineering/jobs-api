11c692e64a311e7658bf0b75a37f0ee5a846ac40
Microsoft Learn Student <learn@contoso.com>
2022-03-10 15:49:37 +0200

    fix(Config, Logger): replaced logging with logrus. Fixed envs.

--- src/config/config.go
+++ src/config/config.go
@@ -1,29 +1,51 @@
 package config
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	"github.com/spf13/viper"
+	"sync"
+	"strings"
+	log "github.com/sirupsen/logrus"
 )
 
-type Configuration struct {
-	Host        string `json:"host"`
-	Port        string `json:"port"`
-	BasePath    string `json:"base_path"`
-	LogFilename string `json:"log_filename"`
+var (
+	once sync.Once
+	instance Configuration
+)
+
+const config_filename = "config"
+
+func Config(config_path string) Configuration {
+	once.Do(func() {
+		instance = loadConfig(config_path)
+	})
+
+	return instance
 }
 
-func LoadConfig(path string) Configuration {
-	file, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Fatal("Config File Missing. ", err)
+func loadConfig(path string) Configuration {
+	var config_instance Configuration
+
+	// Set the file name, path, type of the configurations file
+	viper.SetConfigName(config_filename)
+	viper.AddConfigPath(path)
+	viper.SetConfigType("yaml")
+
+	// Getting evironment variables
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.WithFields(log.Fields{
+			"Error": err,
+		}).Error("Error reading config file")
 	}
 
-	var config Configuration
-	err = json.Unmarshal(file, &config)
+	err := viper.Unmarshal(&config_instance)
 	if err != nil {
-		log.Fatal("Config Parse Error: ", err)
+		log.WithFields(log.Fields{
+			"Error": err,
+			}).Error("Unable to decode into struct")
 	}
 
-	return config
+	return config_instance
 }
