98aa9d070e788de044e134cc13fa0bc28856c69f
2upo <vladvodopad26@gmail.com>
2022-03-15 16:23:44 +0200

    fix(logs, config): adopted config for db

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"kinza/config"
 	"kinza/docs"
 	"kinza/router"
@@ -13,7 +15,7 @@ import (
 )
 
 // InitApp ...
-func InitApp() *gin.Engine {
+func InitApp() (*gin.Engine, error) {
 	// Gin Init
 	app := gin.New()
 	app.Use(gin.Recovery())
@@ -23,14 +25,21 @@ func InitApp() *gin.Engine {
 	log.Info("Config set up successfully!")
 
 	// Logger init
+	var logLevel log.Level
 	if conf.Gin.Mode == "release" {
 		app.Use(utils.Logger_JSON(conf.Log.Filename))
+		logLevel = log.InfoLevel
 	} else if conf.Gin.Mode == "debug" {
 		gin.ForceConsoleColor()
 		app.Use(gin.Logger())
+		logLevel = log.DebugLevel
 	} else if conf.Gin.Mode == "testing" {
 		gin.ForceConsoleColor()
+		logLevel = log.ErrorLevel
+	} else {
+		return nil, errors.New("gin.mode is invalid")
 	}
+	log.SetLevel(logLevel)
 
 	// Router Init
 	baseGroup := app.Group(conf.Server.BasePath)
@@ -42,13 +51,21 @@ func InitApp() *gin.Engine {
 	docs.SwaggerInfo.BasePath = conf.Server.BasePath
 	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
-	return app
+	return app, nil
 }
 
 func main() {
 	conf := config.Config()
-	app := InitApp()
+
+	app, err := InitApp()
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Serving Application
+	log.WithFields(log.Fields{
+		"Conf": fmt.Sprintf("%#v\n", conf),
+	}).Debug("Config for startup")
 	log.Info("Starting application...")
+
 	app.Run(conf.Server.Host + ":" + conf.Server.Port)
 }
